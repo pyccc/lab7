0c9a3e118d3ed1342d23838d600228e07d7ec3f5
pyccc <42711563+pyccc@users.noreply.github.com>
2020-02-16 00:16:35 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const e = require("express");
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -9,5 +11,13 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	console.log($('.likeBtn'));
+	$('.likeBtn').click(clickLikeBtn);
+	
+}
+
+function clickLikeBtn(){
+	console.log("calling");
+	ga('create', 'UA-158628409-1', 'auto');
+	ga("send", "event", 'like', 'click');
 }
